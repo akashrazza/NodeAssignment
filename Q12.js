4142938bebbcd02d585e37ed46f24cf540b61a65
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 14:07:53 +0530

    Update Q12.js

--- Q12.js
+++ Q12.js
@@ -1,6 +1,6 @@
 var os = require('os');
 console.log("Home Directory : ",os.homedir())
- console.log("Network Interface : ",JSON.stringify(os.networkInterfaces()))
+console.log("Network Interface : ",JSON.stringify(os.networkInterfaces()))
 console.log("OS Platform : ",os.platform())
 console.log("Release of the OS : ",os.release())
 console.log("Temporary directory : ",os.tmpdir())
